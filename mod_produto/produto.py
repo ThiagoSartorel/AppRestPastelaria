ae7a5767d1c0ef6580ba85c2441a36375c1b26e3
Thiago Sartorel <thiagosartorel@uniplac.net>
2022-10-13 15:50:16 -0300

    Atividade layout

--- mod_produto/produto.py
+++ mod_produto/produto.py
@@ -3,9 +3,13 @@ from flask import Blueprint, render_template
 bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_folder='templates')
 
 ''' rotas dos formulários '''
-@bp_produto.route('/')
+@bp_produto.route('/', methods=['GET', 'POST'])
 def formListaProduto():
     return render_template('formListaProduto.html'), 200
+
+@bp_produto.route('/form-produto', methods=['GET', 'POST'])
+def formProduto():
+    return render_template('formProduto.html'), 200
 
 '''
 Rota antiga de app...
