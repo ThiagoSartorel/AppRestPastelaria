ad8c48973813e5291a8b37f599268f65fc2208f1
Thiago Sartorel <thiagosartorel@uniplac.net>
2022-11-23 11:18:09 -0300

    atv2311

--- mod_cliente/cliente.py
+++ mod_cliente/cliente.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 import requests
 from funcoes import Funcoes
 
@@ -46,10 +46,59 @@ def insert():
     except Exception as e:
         return render_template('formListaCliente.html', msgErro=e)
 
-'''
-Rota antiga de app...
-@app.route('/funcionario/')
-def formListaFuncionario():
-    # return "<h1>Rota da página de Funcionários da nossa WEB APP</h1>", 200
-    return render_template('formListaFuncionario.html'), 200
-'''
+@bp_cliente.route("/form-edit-cliente", methods=['POST'])
+def formEditCliente():
+    try:
+        # ID enviado via FORM
+        id_cliente = request.form['id']
+        # executa o verbo GET da API buscando somente o funcionário selecionado,
+        # obtendo o JSON do retorno
+        response = requests.get(urlApiClientes + id_cliente, headers=headers)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+            # renderiza o form passando os dados retornados
+        return render_template('formCliente.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+    
+@bp_cliente.route('/edit', methods=['POST'])
+def edit():
+    try:
+        # dados enviados via FORM
+        id_cliente = request.form['id']
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        dia_fiado = 5
+        compra_fiado = True
+        senha = Funcoes.cifraSenha(request.form['senha'])
+        # monta o JSON para envio a API
+        payload = {'id_cliente': id_cliente, 'nome': nome,'cpf': cpf, 'telefone': telefone, 'dia_fiado': dia_fiado, 'senha': senha, 'compra_fiado': compra_fiado}
+
+        # executa o verbo PUT da API e armazena seu retorno
+        response = requests.put(urlApiClientes + id_cliente, headers=headers, json=payload)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 201):
+            raise Exception(result[0])
+        return redirect( url_for('cliente.formListaCliente', msg=result[0]) )
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+    
+@bp_cliente.route('/delete', methods=['POST'])
+def delete():
+    try:
+        # dados enviados via FORM
+        id_cliente = request.form['id_cliente']
+        # executa o verbo DELETE da API e armazena seu retorno
+        response = requests.delete(urlApiClientes + id_cliente, headers=headers)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 201):
+            raise Exception(result[0])
+        return redirect(url_for('cliente.formListaCliente', msg=result[0]))
+        #print(result[0])
+        #{'msg': 'Excluído com sucesso!', 'id': 4}
+        #return jsonify(erro=False, msg=result[0])
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+        #return jsonify(erro=True, msgErro=e.args[0])
